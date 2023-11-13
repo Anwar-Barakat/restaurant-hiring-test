bdf9c98eabbc0e030eadcf1cce5a767d3d8dd915
Anwar-Barakat <brkatanwar0@gmail.com>
2023-11-13 11:29:23 +0400

    added categories, products dummy data.

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -11,9 +11,145 @@ use Illuminate\Support\Str;
 class CategorySeeder extends Seeder
 {
     /**
+     * 
      * Run the database seeds.
      */
     public function run(): void
     {
+        $faker = Factory::create();
+        $categories = [
+            [
+                'id'                => 1,
+                'parent_id'         => 0,
+                'name'              => 'Seafood',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Seafood', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            [
+                'id'                => 2,
+                'parent_id'         => 0,
+                'name'              => 'Meat',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Meat', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            [
+                'id'                => 3,
+                'parent_id'         => 0,
+                'name'              => 'Vegetables',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Vegetables', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            // ____________
+            [
+                'parent_id'         => 1,
+                'name'              => 'Fish',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Fish', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            [
+                'parent_id'         => 1,
+                'name'              => 'Shrimp',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Shrimp', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            [
+                'parent_id'         => 1,
+                'name'              => 'Salmon',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Salmon', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            // ____________
+            [
+                'parent_id'         => 2,
+                'name'              => 'Beef',
+                'discount'          => 5,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Beef', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            [
+                'parent_id'         => 2,
+                'name'              => 'Chicken',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Chicken', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            [
+                'parent_id'         => 2,
+                'name'              => 'Lamb',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Lamb', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            // ____________
+            [
+                'parent_id'         => 3,
+                'name'              => 'Leafy vegetables',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Leafy vegetables', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            [
+                'parent_id'         => 3,
+                'name'              => 'Root vegetables',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Root vegetables', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            [
+                'parent_id'         => 3,
+                'name'              => 'Fruit vegetables',
+                'discount'          => 0,
+                'description'       => $faker->sentence(130),
+                'url'               => Str::slug('Fruit vegetables', '-'),
+                'meta_title'        => $faker->words(3, true),
+                'meta_description'  => $faker->sentence(50),
+                'meta_keywords'     => $faker->sentence(15),
+            ],
+            // ____________
+        ];
+
+        foreach ($categories as $category) {
+            if (is_null(Category::where('url', $category['url'])->first()))
+                Category::create($category);
+        }
     }
 }
--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -15,5 +15,136 @@ class ItemSeeder extends Seeder
      */
     public function run(): void
     {
+        $faker = Factory::create();
+
+        $chicken            = Category::select('id')->where('url', 'chicken')->first();
+        $meat               = Category::select('id')->where('url', 'meat')->first();
+        $leafyVegetables    = Category::select('id')->where('url', 'leafy-vegetables')->first();
+
+        $items              = [
+            [
+                'category_id'       => $chicken->id,
+                'name'              => 'Roasted chicken',
+                'price'             => rand(10, 100),
+                'discount'          => 0,
+                'description'       => $faker->sentence(120),
+                'meta_title'        => $faker->sentence(3),
+                'meta_description'  => $faker->sentence(10),
+                'meta_keywords'     => $faker->sentence(5),
+                'is_featured'       => 1,
+                'is_best_seller'    => true,
+            ],
+            [
+                'category_id'       => $chicken->id,
+                'name'              => 'Grilled chicken',
+                'price'             => rand(230, 260),
+                'discount'          => 0,
+                'description'       => $faker->sentence(120),
+                'meta_title'        => $faker->sentence(3),
+                'meta_description'  => $faker->sentence(10),
+                'meta_keywords'     => $faker->sentence(5),
+                'is_featured'       => 1,
+                'is_best_seller'    => true,
+            ],
+            [
+                'category_id'       => $meat->id,
+                'name'              => 'Grilled steak',
+                'price'             => rand(450, 500),
+                'discount'          => 0,
+                'description'       => $faker->sentence(120),
+                'meta_title'        => $faker->sentence(3),
+                'meta_description'  => $faker->sentence(10),
+                'meta_keywords'     => $faker->sentence(5),
+                'is_featured'       => 1,
+                'is_best_seller'    => true,
+            ],
+            [
+                'category_id'       => $meat->id,
+                'name'              => 'Kebab',
+                'price'             => rand(380, 420),
+                'discount'          => 0,
+                'description'       => $faker->sentence(120),
+                'meta_title'        => $faker->sentence(3),
+                'meta_description'  => $faker->sentence(10),
+                'meta_keywords'     => $faker->sentence(5),
+                'is_featured'       => 1,
+                'is_best_seller'    => true,
+            ],
+            [
+                'category_id'       => $leafyVegetables->id,
+                'name'              => 'Green salad',
+                'price'             => rand(450, 500),
+                'discount'          => 0,
+                'description'       => $faker->sentence(120),
+                'meta_title'        => $faker->sentence(3),
+                'meta_description'  => $faker->sentence(10),
+                'meta_keywords'     => $faker->sentence(5),
+                'is_featured'       => 0,
+                'is_best_seller'    => true,
+            ],
+            [
+                'category_id'       => $leafyVegetables->id,
+                'name'              => 'Fruit salad',
+                'price'             => rand(450, 500),
+                'discount'          => 0,
+                'description'       => $faker->sentence(120),
+                'meta_title'        => $faker->sentence(3),
+                'meta_description'  => $faker->sentence(10),
+                'meta_keywords'     => $faker->sentence(5),
+                'is_featured'       => 0,
+                'is_best_seller'    => true,
+            ],
+            [
+                'category_id'       => $leafyVegetables->id,
+                'name'              => 'Vegetable soup',
+                'price'             => rand(450, 500),
+                'discount'          => 0,
+                'description'       => $faker->sentence(120),
+                'meta_title'        => $faker->sentence(3),
+                'meta_description'  => $faker->sentence(10),
+                'meta_keywords'     => $faker->sentence(5),
+                'is_featured'       => 0,
+                'is_best_seller'    => true,
+            ],
+            [
+                'category_id'       => $leafyVegetables->id,
+                'name'              => 'Lentil soup',
+                'price'             => rand(450, 500),
+                'discount'          => 0,
+                'description'       => $faker->sentence(120),
+                'meta_title'        => $faker->sentence(3),
+                'meta_description'  => $faker->sentence(10),
+                'meta_keywords'     => $faker->sentence(5),
+                'is_featured'       => 0,
+                'is_best_seller'    => true,
+            ],
+            [
+                'category_id'       => $leafyVegetables->id,
+                'name'              => 'Roasted vegetable platter',
+                'price'             => rand(18, 25),
+                'discount'          => 0,
+                'description'       => $faker->sentence(120),
+                'meta_title'        => $faker->sentence(3),
+                'meta_description'  => $faker->sentence(10),
+                'meta_keywords'     => $faker->sentence(5),
+                'is_featured'       => 0,
+                'is_best_seller'    => true,
+            ],
+            [
+                'category_id'       => $meat->id,
+                'name'              => 'Grilled lamb',
+                'price'             => rand(25, 30),
+                'discount'          => 0,
+                'description'       => $faker->sentence(120),
+                'meta_title'        => $faker->sentence(3),
+                'meta_description'  => $faker->sentence(10),
+                'meta_keywords'     => $faker->sentence(5),
+                'is_featured'       => 0,
+                'is_best_seller'    => true,
+            ],
+        ];
+        foreach ($items as $item) {
+            Item::create($item);
+        }
     }
 }
