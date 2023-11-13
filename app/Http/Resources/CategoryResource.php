014dbf84fbe62d664488407074c17bf941bf1bc5
Anwar-Barakat <brkatanwar0@gmail.com>
2023-11-13 12:26:12 +0400

    The category resource added to customize the returned data

---
+++ app/Http/Resources/CategoryResource.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CategoryResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'                => $this->id,
+            'name'              => $this->name,
+            'url'               => $this->url,
+            'discount'          => $this->discount,
+            'description'       => $this->description,
+            'meta_title'        => $this->meta_title,
+            'meta_description'  => $this->meta_description,
+            'meta_keywords'     => $this->meta_keywords,
+            'is_active'         => $this->is_active ? 'Active' : 'Not-active',
+            'created_at'        => Carbon::parse($this->created_at)->format('Y-m-d'),
+            
+            'parent_category'       => $this->parentCategory ? $this->parentCategory->name : 'Parent Category',
+            'children_categories'   => $this->subCategories ?? '-',
+        ];
+    }
+}
--- routes/api/v1/categories.php
+++ routes/api/v1/categories.php
@@ -1,12 +1,14 @@
 <?php
 
+use App\Http\Resources\CategoryResource;
+use App\Models\Category;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/categories', function (Request $request) {
-    return new JsonResource([
-        'data' => 'aaa'
-    ]);
+    $pageSize = $request->page_size ?? 20;
+    $products = Category::query()->paginate($pageSize);
+    return CategoryResource::collection($products);
 });
